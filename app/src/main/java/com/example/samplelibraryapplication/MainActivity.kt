c9c166ba3fa63db971cba4c956341f980683f5a2
Paras Dhawan <paras.dhawan17@gmail.com>
2021-10-29 12:17:44 +0530

    Added Custom Dialog Module

--- app/src/main/java/com/example/samplelibraryapplication/MainActivity.kt
+++ app/src/main/java/com/example/samplelibraryapplication/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.samplelibraryapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.sillysdk.SillySDK
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
